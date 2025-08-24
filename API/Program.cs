91f41ce325f3a57e15b92bf98a3b7894cc56349a
NemanjaTomic57 <nemanja.tomic@ik.me>
2025-08-24 16:16:33 +0000

    fix cors

--- API/Program.cs
+++ API/Program.cs
@@ -1,21 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-var Cors = "_cors";
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(
-        name: Cors,
-        policy =>
-        {
-            policy.WithOrigins("http://127.0.0.1:3000", "https://tonit.dev", "https://www.tonit.dev").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-        }
-    );
-});
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -25,7 +14,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseCors(Cors);
+app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+    .WithOrigins("http://127.0.0.1:3000", "https://tonit.dev", "https://www.tonit.dev"));
 
 app.UseAuthorization();
 
