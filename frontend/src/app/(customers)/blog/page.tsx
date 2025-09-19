af8d5de80c33e47f7fc8750c17edd7deefd69d97
Nemanja57 <nemanja.tomic@ik.me>
2025-09-19 12:06:05 +0200

    add metadata

--- frontend/src/app/(customers)/blog/page.tsx
+++ frontend/src/app/(customers)/blog/page.tsx
@@ -7,8 +7,6 @@ import Link from 'next/link';
 
 export default async function BlogPage() {
     const blogPosts = await axios.get(apiUrl + 'blog/get-all').then((r) => r.data as Blog[]);
-    const imgSrc =
-        'https://images.unsplash.com/photo-1757252800867-2e78e08a6d53?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
     return (
         <div className="container-sm py-container-sm-vert">
@@ -16,7 +14,7 @@ export default async function BlogPage() {
                 {blogPosts.map((blogPost) => (
                     <Link key={blogPost.id} href={'blog/' + blogPost.slug} className="flex flex-col">
                         <div className="relative mb-4 aspect-12/8 overflow-hidden rounded-lg">
-                            <Image src={imgSrc} alt="Thumbnail" fill className="object-cover" />
+                            <Image src={blogPost.thumbnailSlug} alt="Thumbnail" fill className="object-cover" />
                         </div>
                         <h5>{blogPost.heading}</h5>
                         <div className="mt-auto">
