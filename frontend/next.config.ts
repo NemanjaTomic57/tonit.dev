db04b6b6e8fd2f4822cd9fa44514fc1fba05d5b1
Nemanja57 <nemanja.tomic@ik.me>
2025-09-20 13:41:30 +0200

    update config for image hosting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ['storage.googleapis.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+        unoptimized: true,
     },
 };
 
